fca22807a529950864ae69996b9f863eb89a94d6
Barnold88 <bandslover@outlook.com>
2026-02-16 23:57:18 +0000

    Add powershell file to be wrapper for openssl encryption decryption for files

---
+++ Windows/enc_dcr.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Mandatory=$true)][bool]$method,
+    [Parameter(Mandatory=$true)][string]$file,
+    [Parameter(Mandatory=$false)][switch]$clipboard = $false
+)
+
+if ( $method ){
+    openssl enc -aes-256-cbc -in "$($file)" -out "$($file).encrypted"
+}
+else{
+    if ( $clipboard )
+    {
+        #TODO: copy contents of file to clipboard and delete file 
+        openssl enc -d -aes-256-cbc -in $file -out "$($file).unencrypted"
+        type "$($file).unencrypted" | clip.exe
+        del /f "$($file).unencrypted"
+    }else{
+        openssl enc -d -aes-256-cbc -in $file -out "$($file).unencrypted"
+    }
+
+}
