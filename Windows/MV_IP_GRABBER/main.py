87d7e487055458d1e09bc1b6b7412c263d8297a5
Barnold88 <bandslover@outlook.com>
2026-04-11 23:47:59 +0100

    Add logic to parse through ipconfig strings to grab the ipv4 address

--- Windows/MV_IP_GRABBER/main.py
+++ Windows/MV_IP_GRABBER/main.py
@@ -2,9 +2,14 @@
 
 import subprocess
 import sys
+import re
+import pyperclip
+
 
 MULLVAD_NAME = "mullvad"
-
+IPV4 = "IPv4 Address"
+IP_REGEX = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+IP_MIN_CHAR_LEN = 7
 
 def eprint(*args, **kwargs): # with thanks to https://stackoverflow.com/questions/5574702/how-do-i-print-to-stderr-in-python 
     print(*args, file=sys.stderr, **kwargs)
@@ -14,14 +19,20 @@ if __name__ == "__main__":
     ip_text = subprocess.check_output(['ipconfig']).decode("utf-8") # call ipconfig and convert to string from bytes
     strings = ip_text.split("\n")
     mullvad_string = ""
+    found = False
 
-    for word in strings:
-        if MULLVAD_NAME in word.lower():
-            mullvad_string = word
+    for newline in strings:
+        lowered = newline.lower()
 
-    if len(mullvad_string) <= 0:
-        eprint("Error: could not find mullvad network adapter in ipconfig command") 
-    
+        if mullvad_string in lowered:
+            found = True
 
+        if found and "ipv4 address" in lowered:
+            match = IP_REGEX.search(newline)
+            if match:
+                mullvad_string = match.group()
 
+        if found:
+            found = False
 
+    pyperclip.copy(mullvad_string)
