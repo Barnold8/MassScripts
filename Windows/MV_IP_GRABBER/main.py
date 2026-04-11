163bf40f4e6ee8de3ea56949096264ec257d6f75
Barnold88 <bandslover@outlook.com>
2026-04-11 23:35:56 +0100

    Add detection for mullvad network adatper

--- Windows/MV_IP_GRABBER/main.py
+++ Windows/MV_IP_GRABBER/main.py
@@ -3,7 +3,7 @@
 import subprocess
 import sys
 
-MULLVAD_NAME = "Mullvad"
+MULLVAD_NAME = "mullvad"
 
 
 def eprint(*args, **kwargs): # with thanks to https://stackoverflow.com/questions/5574702/how-do-i-print-to-stderr-in-python 
@@ -13,6 +13,15 @@ if __name__ == "__main__":
 
     ip_text = subprocess.check_output(['ipconfig']).decode("utf-8") # call ipconfig and convert to string from bytes
     strings = ip_text.split("\n")
+    mullvad_string = ""
+
+    for word in strings:
+        if MULLVAD_NAME in word.lower():
+            mullvad_string = word
+
+    if len(mullvad_string) <= 0:
+        eprint("Error: could not find mullvad network adapter in ipconfig command") 
+    
 
 
 
