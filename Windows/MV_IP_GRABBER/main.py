703b0536dbfae4cd7db12731f18754aa8c7dc3fd
Barnold88 <bandslover@outlook.com>
2026-04-11 23:30:41 +0100

    change os.system to subprocess to get output of terminal and not process exit code

--- Windows/MV_IP_GRABBER/main.py
+++ Windows/MV_IP_GRABBER/main.py
@@ -1,17 +1,22 @@
 # Script to get IPV4 address for mullvad VPN connection so I can bind it to my torrenting software easily in Windows
 
-import os 
+import subprocess
 import sys
 
 MULLVAD_NAME = "Mullvad"
 
+
+def eprint(*args, **kwargs): # with thanks to https://stackoverflow.com/questions/5574702/how-do-i-print-to-stderr-in-python 
+    print(*args, file=sys.stderr, **kwargs)
+
 if __name__ == "__main__":
 
-    ip_text = os.system("ipconfig")
+    ip_text = subprocess.check_output(['ipconfig']).decode("utf-8") # call ipconfig and convert to string from bytes
     
     try:
         mullvad_start = ip_text.index(MULLVAD_NAME)
+        print(mullvad_start)
     except ValueError as notFound:
-        print()
+        eprint("Error:", notFound)
 
 
